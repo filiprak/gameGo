66725aaa7850c27b882e54a38a8c4fdc0447b50a
gepard5 <michal.glinka95@gmail.com>
2017-01-04 23:24:53 +0100

    Modified board class
    Added few methods:
    *deleting stone
    *checkig if stone has breath
    Added attribute for crosses where stones can't be put
    Attributes for current turn and if player still moves.

--- src/pl/edu/pw/elka/pszt/goGame/model/Board.java
+++ src/pl/edu/pw/elka/pszt/goGame/model/Board.java
@@ -7,7 +7,9 @@ public class Board {
 	public static final int BOARDSIZE = 5;
 	public static final char WHITESGN = 'W', BLACKSGN = 'B', EMPTYSGN = '+';
 	
-	private int whiteStones, blackStones;
+	private int whiteStones, blackStones, whiteNonstones, blackNonstones;
+	private boolean whiteMoves, blackMoves;
+	private char currentTurn;
 	
 	public Board() {
 		whiteStones = 0x00000000;
@@ -22,8 +24,29 @@ public class Board {
 		
 		if (color == WHITESGN) {
 			whiteStones |= (1 << position);
-		} else if(color == BLACKSGN){
+		} 
+		else if(color == BLACKSGN){
 			blackStones |= (1 << position);
+		}
+		
+		if( currentTurn == WHITESGN ) {
+			currentTurn = blackMoves ? BLACKSGN : WHITESGN;
+		} 
+		else if( currentTurn == BLACKSGN ) {
+			currentTurn = whiteMoves ? WHITESGN : BLACKSGN;
+		}
+	}
+	
+	public void deleteStone(int position) throws RuntimeException {
+		if(position > BOARDSIZE * BOARDSIZE || position < 1)
+			throw new RuntimeException("Trying to delete a stone outside the board !");
+		if(((whiteStones >> position) & 1) == 0 && ((blackStones >> position) & 1) == 0 )
+			throw new RuntimeException("Trying to delete a stone from empty cross !");
+		
+		if (currentTurn == WHITESGN) {
+			whiteStones &= ~(1 << position);
+		} else if(currentTurn == BLACKSGN){
+			blackStones &= ~(1 << position);
 		} else
 			return;
 	}
@@ -31,17 +54,44 @@ public class Board {
 	public void clear() {
 		whiteStones = 0x00000000;
 		blackStones = 0x00000000;
+		whiteNonstones = 0x00000000;
+		blackNonstones = 0x00000000;
 	}
 	
-	public List<Integer> getValidMoves(char color) {
-		List<Integer> validMoves = new ArrayList<Integer>();
+	boolean hasBreath( int position, int allStones ) {
+		int breathMask = 0x000010A2; //0100010100001000...
+		int leftSideMask = 0x00108421; //1000010000100001000010000...
+		int rightSideMask = 0x01084210; //0000100001000010000100001...
+		int bottomSideMask = 0x01F00000; //000000000000000000001111100...
+		int topSideMask = 0x0000001F; //111110000...
+		
+		if( (( 1 << position ) & leftSideMask) == 1 ) {
+			breathMask &= ~leftSideMask;
+		}
+		if( (( 1 << position ) & rightSideMask) == 1 ) {
+			breathMask &= ~rightSideMask;
+		}
+		if( (( 1 << position ) & bottomSideMask) == 1 ) {
+			breathMask &= ~bottomSideMask;
+		}
+		if( (( 1 << position ) & topSideMask) == 1 ) {
+			breathMask &= ~topSideMask;
+		}
+		
+		breathMask = breathMask << position;
+		return (allStones & breathMask) != 0;
+		
+	}
+	
+	
+	int getValidMoves(char color) {
+		int validMoves = 0;
 		int allStones = whiteStones | blackStones;
 		
 		for (int i = 1; i < BOARDSIZE * BOARDSIZE + 1; ++i) {
 			if(((allStones >> i) & 1) == 1) // skip all occupied positions
 				continue;
 			// todo reszta przypadkow
-			validMoves.add(i);
 		}
 		return validMoves;
 	}
