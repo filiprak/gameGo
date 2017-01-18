879dc01f26e68faf8379a207d765780be10a64f8
gepard5 <michal.glinka95@gmail.com>
2017-01-18 20:12:22 +0100

    Added missing file

---
+++ src/pl/edu/pw/elka/pszt/goGame/view/AIOptions.java
@@ -0,0 +1,11 @@
+package pl.edu.pw.elka.pszt.goGame.view;
+
+
+public class AIOptions {
+	public int simulations;
+	public int children_limit;
+	public int children_limit_jump;
+	public double exploration_ratio;
+	public double koi_points;
+	public char newGameColor;
+}
